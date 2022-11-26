3644b0d83efeaebaf53db1157c6e823a8fd83248
Loraine Vilches <lorainevilches@gmail.com>
2022-11-26 02:36:50 +0100

    security + project-name change
    Remove Firebase Token
    Change Project Name to share

--- src/firebase.js
+++ src/firebase.js
@@ -7,12 +7,12 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA0SZF0vu6XQgSp5CJDgOjV0Czz-xTlIOM",
-  authDomain: "frat-authentication.firebaseapp.com",
-  projectId: "frat-authentication",
-  storageBucket: "frat-authentication.appspot.com",
-  messagingSenderId: "697805542885",
-  appId: "1:697805542885:web:0de20a6e6aa99e51e88b0c"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
